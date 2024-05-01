06c68e505657ed49f9a0e1e7424e13f6342e71d1
LuisBMI <luisbmi55@gmail.com>
2024-05-01 20:45:50 +0200

    feat: added model description

--- src/modelling/BurgerRestModel.java
+++ src/modelling/BurgerRestModel.java
@@ -67,7 +67,15 @@ public class BurgerRestModel extends Model {
      * @return model description as a string
      */
     public String description() {
-        return "This model describes a... Complete this description...";
+        return "This model describes a restaurant where clients arrive to a single queue "
+                + "and have the order taken by the dependientes in order of arrival. "
+                + "When an order is taken by a dependiente, it waits in the busy dependientes "
+                + "Queue until the cocinero is free and can start cooking the order. "
+                + "Once the order has been cooked the client pays for it and leaves.\n"
+                + "The clients leave the waiting queue when they have their order taken by the dependiente.\n"
+                + "The dependientes do not serve any other customer until payment has been made.\n"
+                + "The cooks exclusively prepare a customer's order and until they have finished " 
+                + "with the entire order they do not start the next one.";
     }
 
     // Creates the first event to kickoff the simulation
